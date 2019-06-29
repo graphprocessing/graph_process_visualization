c692e9056086546a66f7f95071e88f382bc1b3e5
Arseniy Obolenskiy <gooddoog@student.su>
2019-06-29 10:08:14 +0300

    Fixes after review

--- app/main.cpp
+++ app/main.cpp
@@ -2,8 +2,8 @@
 #include <QApplication>
 
 int main(int argc, char *argv[]) {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    QApplication app(argc, argv);
+    MainWindow window;
+    window.show();
+    return app.exec();
 }
